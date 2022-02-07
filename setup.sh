eb1cf8cf1252312c1e5424b8a7d11e61576682c9
richardjcy001 <84157246+richardjcy001@users.noreply.github.com>
2022-02-06 17:47:21 -0800

    Set up FlexFlow ResNet18 Baseline (#1)
    * Update config.linux
    
    * Fix

---
+++ setup.sh
@@ -0,0 +1,10 @@
+conda deactivate
+
+conda activate
+
+module load gcc/7.3.0
+module load cuda/10.0
+module load cudnn/7.6.2
+module load hdf5/1.10.4
+
+export FF_HOME=/work/03924/xuehaiq/maverick2/jcy_workspace/FlexFlow

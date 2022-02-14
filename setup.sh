b7dfbf2fba040bf3d7634cf8d0db871595357c3c
richardjcy001 <richardjiang01@outlook.com>
2022-02-14 08:29:13 -0800

    Upgrade CUDA and CUDNN Libraries to Support ExportStrategy Option

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ conda deactivate
 conda activate
 
 module load gcc/7.3.0
-module load cuda/10.0
-module load cudnn/7.6.2
+module load cuda/11.0
+module load cudnn/8.0.5
 module load hdf5/1.10.4
 
 export FF_HOME=/work/03924/xuehaiq/maverick2/jcy_workspace/FlexFlow
